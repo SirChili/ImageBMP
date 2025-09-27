44b41836ac3084a6dc98f704a47d4897b091ec5b
SirChili <thomastony4628@gmail.com>
2025-09-27 18:47:56 -0500

    Create image.cpp

---
+++ image.cpp
@@ -0,0 +1,52 @@
+#include "image.h"
+#include <iostream>
+#include <fstream>
+
+Color::Color()
+	:r(0), g(0), b(0)
+{
+}
+
+Color::Color(float r, float g, float b)
+	:r(r), g(g), b(b)
+{
+}
+
+Color::~Color()
+{
+}
+
+image::image(int width, int height)
+	: m_width(width), m_height(height), m_colors(std::vector<Color>(width * height))
+{
+}
+
+image::~image()
+{
+}
+
+Color image::GetColor(int x, int y) const
+{
+	return m_colors[y * m_width + x];
+}
+
+void image::SetColor(const Color& color, int x, int y)
+{
+	m_colors[y * m_width + x].r + color.r;
+	m_colors[y * m_width + x].r + color.g;
+	m_colors[y * m_width + x].r + color.b;
+}
+
+void image::Export(const char* path) const
+{
+	ofstream f;
+	f.open(path, ios::out | ios::binary);
+
+	if (!f.is_open()) {
+		cout << "File could not be opened.";
+		return;
+	}
+
+	unsigned char bmpPad[3] = {0,0,0};
+	const int paddingAmount = ((4 - (m_width * 3) % 4) % 4);
+}
