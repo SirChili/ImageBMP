0b23df7a777135ec28479979572843cb1914ff75
SirChili <thomastony4628@gmail.com>
2025-09-30 13:25:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
-#include "image.h";
+#include "image.h"
 
-int main(){
+using namespace std;
 
+int main() {
+
+	const int width = 640;
+	const int height = 480;
+
+	image Image(width,height);
+
+	for (int y = 0; y < height; y++) {
+
+		for (int x = 0; x < width; x++) {
+			Image.SetColor(Color((float)x / (float)width, 1.0f -((float)x / (float)width), (float)y /(float)height), x,y);
+		}
+	}
+	//Currently doesn't work, only displays black
+	Image.Export("image.bmp");
 }
